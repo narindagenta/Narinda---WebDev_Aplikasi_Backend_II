9dd88374c944e3e65aca5f9066c472c006bd99c4
narindagenta <82932789+narindagenta@users.noreply.github.com>
2022-10-05 21:34:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,50 +18,47 @@ databaseConfig.connect((err)=>{
 app.use(express.Router());
 app.use('/', routes)
 
-// app.get('/',(req,res)=>{
-//     db.query(`SELECT * FROM links`,(err,results)=>{
-//         if(err){
-//             console.log(err)
-//             return
-//         }
-//         res.send(results.rows)
-//     })
+app.get('/',(req,res)=>{
+    db.query(`SELECT * FROM links`,(err,results)=>{
+        if(err){
+            console.log(err)
+            return}
+        res.send(results.rows)
+    })
 
-// })
+})
 
-// app.post('/insert',(req,res)=>{
-//     db.query(`INSERT INTO links(url,name,description) 
-//         values('${req.body.url}','${req.body.name}','${req.body.description}')`
-//     ,(err)=>{
-//         if(err){
-//             console.log(err)
-//             return
-//         }
-//         res.send(`URL berhasil ditambahkan ke database`)
-//     })
-// })
+app.post('/insert',(req,res)=>{
+    db.query(`INSERT INTO links(url,name,description) 
+        values('${req.body.url}','${req.body.name}','${req.body.description}')`
+    ,(err)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        res.send(`URL berhasil ditambahkan ke database`)
+    })
+})
+ app.put('/update',(req,res)=>{
+    db.query(`UPDATE links set name='${req.body.name}' where url='${req.body.url}' `,(err)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        res.send(`URL dengan nama ${req.body.name} berhasil diupdate`)
+    })
+})
+app.delete('/delete',(req,res)=>{
+    db.query(`DELETE FROM links where url='${req.body.url}'`,(err)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        res.send(`URL berhasil dihapus`)
+    })
+})
 
-// app.put('/update',(req,res)=>{
-//     db.query(`UPDATE links set name='${req.body.name}' where url='${req.body.url}' `,(err)=>{
-//         if(err){
-//             console.log(err)
-//             return
-//         }
-//         res.send(`URL dengan nama ${req.body.name} berhasil diupdate`)
-//     })
-// })
-
-// app.delete('/delete',(req,res)=>{
-//     db.query(`DELETE FROM links where url='${req.body.url}'`,(err)=>{
-//         if(err){
-//             console.log(err)
-//             return
-//         }
-//         res.send(`URL berhasil dihapus`)
-//     })
-// })
-
-app.listen(5000,()=>{
-    console.log(`Server berjalan di port 5000`)
+app.listen(1019,()=>{
+    console.log(`Server berjalan di port 1019`)
 })
 
